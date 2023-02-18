1eadc069cd7f088d7fa27d73de9472a0e9506583
Enjoywater <heungsoo1@gmail.com>
2023-02-18 16:12:57 +0900

    Add Noto serif font

--- src/styles/font.ts
+++ src/styles/font.ts
@@ -1,7 +1,12 @@
 import { css } from 'styled-components';
 
-export const font = (size: number, weight: number) => css`
+export const font = (size: number, weight: number, isSerif = false) => css`
   font-size: ${size}px;
   font-weight: ${weight};
   line-height: ${size * 1.5}px;
+
+  ${isSerif &&
+  css`
+    font-family: 'Noto Serif', serif;
+  `}
 `;
